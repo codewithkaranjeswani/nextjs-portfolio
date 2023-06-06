6a2b46d3e568a17e5813ac2b41d108c87e235164
Karan Jeswani <krnjeswani21@gmail.com>
2023-06-07 02:10:30 +0530

    added og for link previews

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "@/src/styles/globals.css"
 import Navbar from "@/src/components/Navbar"
 import Footer from "@/src/components/Footer"
 import { ThemeProvider } from "next-themes"
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({
   children,
@@ -12,11 +12,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+      <head>
+        <title>KJo&apos;s World!</title>
+        <meta
+          property="og:image"
+          content="http://karanjeswani.vercel.app/api/og"
+        />
+      </head>
       <body className="bg-stone-200 dark:bg-stone-800 flex flex-col min-h-screen">
         <ThemeProvider enableSystem={true} attribute="class">
           <Navbar />
---
+++ src/pages/api/og.tsx
@@ -0,0 +1,32 @@
+import { ImageResponse } from "@vercel/og";
+
+export const config = {
+  runtime: "edge",
+};
+
+function Func() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 128,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          textAlign: "center",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        KJo&apos;s World!
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600,
+    },
+  );
+}
+
+export default Func;
