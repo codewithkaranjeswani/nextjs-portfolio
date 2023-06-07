62540929d4e2579ce9e958287aac6d2801476767
Karan Jeswani <krnjeswani21@gmail.com>
2023-06-07 14:44:42 +0530

    correcting twitter image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <meta name="twitter:site" content="@karanjeswani306" />
         <meta name="twitter:title" content="👋 Hi I&apos;m Karan!" />
         <meta name="twitter:description" content="Karan Jeswani's Portfolio" />
-        <meta name="twitter:image" content="https//karanjeswani.vercel.app/api/og" />
+        <meta name="twitter:image" content="http://karanjeswani.vercel.app/api/og" />
       </head>
       <body className="bg-stone-200 dark:bg-stone-800 flex flex-col min-h-screen">
         <ThemeProvider enableSystem={true} attribute="class">
