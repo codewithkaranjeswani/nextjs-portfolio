1699fa1ed609262251495dd8c9f63275a69a5a48
Karan Jeswani <krnjeswani21@gmail.com>
2023-06-07 15:21:59 +0530

    adding hi emoji

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -9,11 +9,11 @@ function HeroSection() {
         <div>
           <Image
             className="rounded-full shadow-2xl"
-            src="/faceimg.jpg" alt="my face image"
+            src="/faceimg.png" alt="my face image"
             priority={false} quality={75} loading="lazy" width={300} height={300} />
         </div>
         <div>
-          <h1 className="font-bold text-4xl mt-6">Hi, I&#39;m Karan Jeswani!</h1>
+          <h1 className="font-bold text-4xl mt-6">Hi 👋, I&#39;m Karan Jeswani!</h1>
           <p className="text-lg mt-4 mb-6">I&#39;m a{" "}
             <span className="semi-bold text-teal-900">Machine Learning Engineer{" "}</span>
             interested in applied mathematics and web development!</p>
