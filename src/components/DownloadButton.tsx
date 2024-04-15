f7191290bc6fe48f6c6915ca8261b6f4dea85b38
codewithkaranjeswani <krnjeswani21@gmail.com>
2024-04-15 22:53:28 +0530

    upd downloadbtn and darker dark mode

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -1,44 +1,17 @@
 "use client";
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 import { AiOutlineDownload } from "react-icons/ai";
 
-interface ButtonProp {
-  theme?: string;
-  mounted: boolean;
-}
-
-function DownloadButton() {
-  const { systemTheme, theme, setTheme } = useTheme();
-  const currentTheme = theme === "system" ? systemTheme : theme;
-  const [mounted, setMounted] = useState(false);
-  // useEffect only runs on the client, so now we can safely show the UI
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
+export default function DownloadButton() {
   const handleDownload = () => {
     const link = document.createElement("a");
     link.href = "/karanjeswani_resume.pdf";
     link.download = "karanjeswani_resume.pdf";
     link.click();
   };
-  let color;
-  if (!mounted) {
-    color = "#00000000";
-  } else {
-    if (currentTheme === "light") {
-      color = "black";
-    } else if (currentTheme === "dark") {
-      color = "white";
-    }
-  }
   return (
     <button className="flex" onClick={handleDownload}>
       <div className="pr-2">CV</div>
-      <AiOutlineDownload size={25} color={color} />
+      <AiOutlineDownload size={25} />
     </button>
   );
 }
-
-export default DownloadButton;
